83a2e57ae444496b0d807640152c7aced12c6d12
Jack Sarick <jacksarick@gmail.com>
2016-09-27 20:53:59 -0400

    We're off to the races!

---
+++ app.js
@@ -0,0 +1,58 @@
+// My jQuery custom
+const $ = require("./jackQuery.js");
+
+// The net library
+var net = require('net');
+
+// The app
+var app = {
+
+	client: new net.Socket(),
+
+	// The user's data
+	user_data: {},
+
+	// Dump the user's data
+	dump: function() { console.log(this.user_data); },
+
+	// Record the "new-server" data to the app
+	userData: function(div) {
+		this.user_data.nickname = $("#nickname").value();
+		this.user_data.username = $("#username").value();
+		this.user_data.realname = $("#realname").value();
+	},
+
+	// Dev function to auto-set names
+	hardSet: function(n) {
+		this.user_data.nickname = n;
+		this.user_data.username = n+"-user";
+		this.user_data.realname = n+"-real";
+	},
+
+	// Make new connection with server and port
+	connect: function(server, port) {
+		app.client.connect(port, server, function() {
+			app.client.send('NICK ' + app.user_data.nickname);
+			app.client.send('USER ' + app.user_data.username + ' 8 * ' + app.user_data.realname);
+			console.log('Connected');
+		});
+	},
+
+	parse: function(data) {
+		const base = data.split(":");
+		return [base[0].split(" "), base.slice(1).join(":")];
+	}
+};
+
+// Minor change, to terminate lines with \r\n
+app.client.send = function(data) {
+	client.write(data + "\r\n");
+}
+
+// client.on('data', function(data) {
+// 	console.log('Received: ' + data);
+// });
+
+module.exports = app;
+
+console.log("app loaded");
---
+++ jackQuery.js
@@ -0,0 +1,18 @@
+function Tag(x) {
+	if (x[0] == "#") {
+		this.main = document.getElementById(x.slice(1));
+	}
+	if (x[0] == ".") {
+		this.main = document.getElementsByClassName(x.slice(1));
+	}
+
+	this.set = function(c) { this.main.innerHTML = c; }
+	this.style = function(a, v) { this.main.style[a] = v; }
+	this.append = function(c) { this.main.innerHTML += c; }
+	this.value = function() { return this.main.value; }
+	this.show = function(s) { this.main.style.display = "block"; }
+	this.hide = function(s) { this.main.style.display = "none"; }
+	this.listen = function(a, f) { this.main.addEventListener(a, f, false); }
+}
+
+module.exports = function(name) { return new Tag(name); }
