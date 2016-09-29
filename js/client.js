89653cea876b7d43ef65b9d6dbab9dbbcca611c8
Jack Sarick <jacksarick@gmail.com>
2016-09-29 15:12:49 -0400

    Up/down arrow keys work

--- js/client.js
+++ js/client.js
@@ -11,20 +11,52 @@ var line = 0;    // How far back
 
 // Listen for data going out
 prompt.listen("keyup", function(event) {
+	
 	event.preventDefault();
-	switch(event.keyCode) {
+	const key = event.keyCode;
+	
+	console.log(key);
+	
+	switch(key) {
+		// Enter
 		case 13:
 			// Collect input
 			const input = prompt.value();
 
-			// Push to recent
-			recent.push(input);
+			// Push to recent and reset counter
+			recent.unshift(input);
+			line = 0;
 
 			// Send it off
 			app.client.send(input);
 
 			// Clear prompt
 			prompt.value("");
+			break;
+
+		// Up arrow
+		case 38:
+			// Set prompt to that line
+			prompt.value(recent[line]);
+
+			// Increment line number, if the list goes on
+			line += line < recent.length;
+			break;
+
+		// Down arrow
+		case 40:
+			console.log(line);
+			if (line == 0){
+				prompt.value("");
+			}
+			else{
+				// Decrement, if possible
+				line -= line > 0;
+
+				// Set prompt to that line
+				prompt.value(recent[line]);
+			}
+
 			break;
 	}
 });
