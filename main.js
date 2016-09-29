7da349a6abf92010b4a00dc820da20a4121ab80e
Jack Sarick <jacksarick@gmail.com>
2016-09-28 21:36:47 -0400

    Added custom command. More to come

--- main.js
+++ main.js
@@ -6,13 +6,21 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
+// Module for shortcuts
+const globalShortcut = electron.globalShortcut
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
 	// Create the browser window.
-	mainWindow = new BrowserWindow({width: 800, height: 600})
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		minWidth: 610,
+		minHeight: 560
+	})
 
 	// and load the index.html of the app.
 	mainWindow.loadURL(`file://${__dirname}/pages/index.html`)
@@ -32,7 +40,15 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+	// Spawn a first window
+	createWindow()
+
+	// Register a 'CommandOrControl+N' shortcut listener.
+	globalShortcut.register('CommandOrControl+N', () => {
+		createWindow()
+	})
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -49,4 +65,9 @@ app.on('activate', function () {
 	if (mainWindow === null) {
 		createWindow()
 	}
+})
+
+app.on('will-quit', () => {
+	// Unregister all shortcuts.
+	globalShortcut.unregisterAll()
 })
